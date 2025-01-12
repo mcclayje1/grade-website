e860316a519f13a83e1694e1f9e8d8b16a179e01
Jesse McClay <jessemcclay@Jesses-Air.attlocal.net>
2025-01-12 15:36:43 -0600

    added search pipe

--- grade-website/src/app/components/course-searcher/course-searcher.component.ts
+++ grade-website/src/app/components/course-searcher/course-searcher.component.ts
@@ -1,46 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms'
 import { CommonModule, KeyValuePipe } from '@angular/common';
 import * as GradeData from '../../../../public/assets/grades/results2023-2024.json';
+import { SearchPipe } from '../pipes/search-pipe';
 
 @Component({
   selector: 'app-course-searcher',
-  imports: [RouterOutlet, KeyValuePipe, CommonModule, FormsModule],
+  imports: [KeyValuePipe, CommonModule, FormsModule, SearchPipe],
   templateUrl: './course-searcher.component.html',
   styleUrl: './course-searcher.component.scss',
   standalone: true
 })
 export class CourseSearcherComponent implements OnInit {
-  gradeData: any = GradeData;
   
-  totalCourses: number = 0;
-  searchResults: number = 0;
+  gradeData: any = GradeData;
 
-  currentCoursesFromSearch: number = 0;
+  totalCourses: number = 0;
+  searchResultsCount: number = 0;
 
   userInput: string = ""
-
   infoBoxOpen: boolean = true;
 
   constructor() {}
 
   ngOnInit(): void {
     this.totalCourses = Object.keys(this.gradeData)?.length
-    this.searchResults = this.totalCourses
   }
 
   closeAlert() {
     this.infoBoxOpen = false
-  }
-
-  returnFilteredCourses() {
-    if (this.userInput) {
-      // let filteredResults = this.gradeData.filter((grade:any)=> ) // this.searchResults = Object.keys(this.gradeData)?.length
-      return this.gradeData.filter((grade: any) => {return })
-    }
-    this.searchResults = this.totalCourses
-    return this.gradeData
   }
 
 }
