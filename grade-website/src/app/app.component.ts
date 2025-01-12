45dc890af1685802501dc4dbb592e08c3fe8e415
Jesse McClay <jessemcclay@Jesses-Air.attlocal.net>
2025-01-12 00:13:31 -0600

    add table and data

--- grade-website/src/app/app.component.ts
+++ grade-website/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, KeyValuePipe } from '@angular/common';
+import * as GradeData from '../../public/assets/grades/results2023-2024.json';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, KeyValuePipe, CommonModule],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'grade-website';
+
+  gradeData: any = GradeData;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // console.log("grade data: ", this.gradeData["SAB-300"])
+  }
+
+  receiveUserInput($event: any) {
+    console.log("event: ", event)
+  }
 }
--- grades-csvs/processor.py
+++ grades-csvs/processor.py
@@ -6,7 +6,6 @@ LETTER_GRADES = {"A": 4,"B": 3,"C": 2,"D": 1,"F": 0, "Other":-1}
 # difficulty is defined as the % of the class that received an A. Higher % means the course is easier; lower means harder
 # accumulated and averaged over semesters
 def determine_course_difficulty(semesters: object) -> float:
-    print("COURSE DATA: ", semesters)
     totalScoreCounts: int = 0 # total # of students taking the course that received a grade
     difficultyPercent: float = 0
     semesterCount = 0
@@ -20,9 +19,36 @@ def determine_course_difficulty(semesters: object) -> float:
         if "A" in semesterData:
             semesterCount += 1
             difficultyPercent += (semesterData["A"] / totalScoreCounts) 
+    return round(difficultyPercent / semesterCount, 2) # return average difficulty % across all semesters
 
+def determine_average_gpa(semesters: object) -> float:
+    totalScoreCounts: int = 0 # total # of students taking the course that received a grade
+    averageGPA: float = 0
+    semesterCount = 0
+    for semester, semesterData in semesters.items():
+        totalScoreCounts = 0
+        semesterCount += 1
+        totalScoreCounts += semesterData["A"]
+        totalScoreCounts += semesterData["B"]
+        totalScoreCounts += semesterData["C"]
+        totalScoreCounts += semesterData["D"]
+        totalScoreCounts += semesterData["F"]
+        averageGPA += ((semesterData["A"]*4) + (semesterData["B"]*3) + (semesterData["C"]*2) + (semesterData["D"]*1)) / totalScoreCounts
 
-    return difficultyPercent / semesterCount # return average difficulty % across all semesters
+    return round(averageGPA / semesterCount, 2) # return average difficulty % across all semesters
+
+def determine_course_size(semesters: object) -> int:
+    studentCount: int = 0
+    semesterCount: int = 0
+    for semester, semesterData in semesters.items():
+        semesterCount += 1
+        studentCount += semesterData["A"]
+        studentCount += semesterData["B"]
+        studentCount += semesterData["C"]
+        studentCount += semesterData["D"]
+        studentCount += semesterData["F"]
+        studentCount += semesterData["Other"]
+    return round(studentCount / semesterCount, 2)
 
 def main():
 
@@ -32,7 +58,7 @@ def main():
         reader = csv.reader(csv_file)
         next(reader) #skip header row
         for row in reader:
-            print("row: ", row)
+            # print("row: ", row)
             semester: str = row[0]
             sectionNum: str = row[1]
             department: str = row[2]
@@ -48,29 +74,32 @@ def main():
                 grade_data[courseID] = {}
                 grade_data[courseID]["overview"] = {}
                 grade_data[courseID]["overview"]["department"] = department
+                grade_data[courseID]["overview"]["coursePrefix"] = coursePrefix
+                grade_data[courseID]["overview"]["courseNum"] = courseNum
                 grade_data[courseID]["overview"]["courseTitle"] = courseTitle
-                grade_data[courseID]["overview"]["courseName"] = courseName
+                # grade_data[courseID]["overview"]["courseName"] = courseName
                 grade_data[courseID]["overview"]["difficulty"] = 0
-                grade_data[courseID]["semesters"] = {}
-
-            if semester not in grade_data[courseID]["semesters"]:
-                grade_data[courseID]["semesters"][semester] = {}
-                grade_data[courseID]["semesters"][semester]["A"] = 0
-                grade_data[courseID]["semesters"][semester]["B"] = 0
-                grade_data[courseID]["semesters"][semester]["C"] = 0
-                grade_data[courseID]["semesters"][semester]["D"] = 0
-                grade_data[courseID]["semesters"][semester]["F"] = 0
-                grade_data[courseID]["semesters"][semester]["Other"] = 0
+                grade_data[courseID]["overview"]["avgGPA"] = 0
+                grade_data[courseID]["overview"]["avgSize"] = 0
+                grade_data[courseID]["sems"] = {}
+            if semester not in grade_data[courseID]["sems"]:
+                grade_data[courseID]["sems"][semester] = {}
+                grade_data[courseID]["sems"][semester]["A"] = 0
+                grade_data[courseID]["sems"][semester]["B"] = 0
+                grade_data[courseID]["sems"][semester]["C"] = 0
+                grade_data[courseID]["sems"][semester]["D"] = 0
+                grade_data[courseID]["sems"][semester]["F"] = 0
+                grade_data[courseID]["sems"][semester]["Other"] = 0
             
             # ensures grade counts get accumulated across sections
-            if (courseLetterGrade in LETTER_GRADES and courseLetterGrade in grade_data[courseID]["semesters"][semester]):
-                grade_data[courseID]["semesters"][semester][courseLetterGrade] += courseGradeAmount
+            if (courseLetterGrade in LETTER_GRADES and courseLetterGrade in grade_data[courseID]["sems"][semester]):
+                grade_data[courseID]["sems"][semester][courseLetterGrade] += courseGradeAmount
 
         for courseID, semesters in grade_data.items():
-            semesters["overview"]["difficulty"] = determine_course_difficulty(semesters=semesters["semesters"])
             # difficulty is defined as the % of the class that received an A. Higher % means the course is easier; lower means harder
-            # grade_data[courseID]["overview"]["difficulty"] = determine_course_difficulty(courseData=courseData)
-
+            semesters["overview"]["difficulty"] = determine_course_difficulty(semesters=semesters["sems"])
+            semesters["overview"]["avgGPA"] = determine_average_gpa(semesters=semesters["sems"])
+            semesters["overview"]["avgSize"] = determine_course_size(semesters=semesters["sems"])
 
         json_object = json.dumps(grade_data, indent=4)
  
